8460694f3e11740bfab68c46ef697db3ddd24b72
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-02-22 18:24:29 +0000

    add __GLIBR__ macro

--- include/glib.h
+++ include/glib.h
@@ -1,6 +1,15 @@
 #ifndef __G_LIB_H__
 #define __G_LIB_H__
 
+
+/*
+ * If one really wants to know if they're using GLibR
+ * 
+ * They shouldn't need this; GLibR should be the one supporting
+ * them not the otherway around
+ */
+#define __GLIBR__
+
 #include <glib/gmacros.h>
 #include <glib/glist.h>
 #include <glib/gmessages.h>
