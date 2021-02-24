4a7f017b008c3e6acfa8f8e484648cd09b790ba8
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-02-24 12:47:33 +0000

    Added more string functions

--- glib/gstring.c
+++ glib/gstring.c
@@ -9,7 +9,7 @@ GString *g_string_new(gchar const *str) {
 	if (str) {
 		ret->len=strlen(str);
 		ret->str=malloc(ret->len+1);
-		strncpy(ret->str, str,strlen(str));
+		strncpy(ret->str, str, ret->len);
 	} else {
 		ret->len=0;
 		ret->str=NULL;
@@ -17,6 +17,44 @@ GString *g_string_new(gchar const *str) {
 	ret->allocated_len=ret->len;
 	return ret;
 };
+
+GString *g_string_new_len(gchar const *str, gssize len) {
+	if (!str) {
+		return NULL
+	}
+	GString *ret = malloc(sizeof(GString));
+	ret->len = len;
+	ret->str = malloc(ret->len + 1);
+	strncpy(ret->str, str, ret->len);
+	ret->str[ret->len] = '\0';
+	ret->allocated_len=ret->len;
+	return ret;
+}
+
+GString *g_string_sized_new(gssize size) {
+	GString *ret = malloc(sizeof(GString));
+	ret->len = 0;
+	ret->str = malloc(size);
+	ret->allocated_len = 0;
+	return ret;
+}
+
+GString *g_string_assign(GString *string, gchar const *src) {
+	if (ret->allocated_len < strlen(src)) {
+		ret->allocated_len = strlen(src);
+		ret->str = realloc(ret->str, strlen(src) + 1);
+	}
+	ret->len = strlen(src);
+	strncpy(ret->str, str, ret->len);
+}
+
+/*
+ * TODO: expand string?
+ */
+GString *g_string_vprintf(GString *string, gchar const *fmt, va_list args) {
+	vsprintf(string->str, fmt, args);
+	return string;
+}
 
 gchar *g_string_free(GString *str, gboolean free_segment) {
 	gchar *ret = str->str;
